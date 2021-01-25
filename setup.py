94229b10b3fb64375ad4e5cf43fdb58e90c1a2b2
crypto <dhiadah@gmail.com>
2021-01-25 22:58:25 +0100

    version 1.2.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/cryptolake/Graff'
 EMAIL = 'dhiadah@gmail.com'
 AUTHOR = 'Dhia'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '1.2'
+VERSION = '1.2.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
